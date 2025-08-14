93bb920c3d123ae971dac6b3ce288a768b27dbbb
Jadna Santos <jadnajesus7@gmail.com>
2025-08-14 14:05:19 -0300

    feat: routes para tratar das rotas da aplicacao

---
+++ internal/api/routes.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func (api *Api) BindRoutes() {
+	api.Router.Use(
+		middleware.RequestID,
+		middleware.Recoverer,
+		middleware.Logger,
+		api.Sessions.LoadAndSave,
+	)
+
+	api.Router.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			r.Route("/users", func(r chi.Router) {
+				r.Post("/signup", api.handleSignupUser)
+				r.Post("/sign", api.handleSignUser)
+				r.With(api.AuthMiddleware).Post("/logout", api.handleLogoutUser)
+
+			})
+		})
+	})
+
+}
