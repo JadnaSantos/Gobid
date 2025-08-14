2f83e58db3067955aa15c6042bbcd27ab5a633de
Jadna Santos <jadnajesus7@gmail.com>
2025-08-14 14:00:19 -0300

    feat: add userService, sessions, cookies e router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/gob"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/pgxstore"
+	"github.com/google/uuid"
+
+	"github.com/JadnaSantos/Gobid.git/internal/api"
+	"github.com/JadnaSantos/Gobid.git/internal/services"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello Worl")
+	gob.Register(uuid.UUID{})
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("GOBID_DATABASE_USER"),
+		os.Getenv("GOBID_DATABASE_PASSWORD"),
+		os.Getenv("GOBID_DATABASE_HOST"),
+		os.Getenv("GOBID_DATABASE_PORT"),
+		os.Getenv("GOBID_DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	s := scs.New()
+	s.Store = pgxstore.New(pool)
+	s.Lifetime = 24 * time.Hour
+	s.Cookie.HttpOnly = true
+	s.Cookie.SameSite = http.SameSiteLaxMode
+
+	api := api.Api{
+		Router:      chi.NewMux(),
+		UserService: services.NewUserService(pool),
+		Sessions:    s,
+	}
+
+	api.BindRoutes()
+
+	port := os.Getenv("GOBID_APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Starting Server on port :%s\n", port)
+	if err := http.ListenAndServe(":"+port, api.Router); err != nil {
+		panic(err)
+	}
 }
